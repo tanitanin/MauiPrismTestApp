a65f3279c5c12b6d34942e2ae888868218165d93
tanitanin <tajima2210@gmail.com>
2023-09-16 04:04:24 +0900

    First Commit

---
+++ MauiPrismTestApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiPrismTestApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiPrismTestApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiPrismTestApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
